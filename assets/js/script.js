671dc63d2728aac3c88069b96f676a18002d9a46
Micheal Ellis <mellis.eriecon@gmail.com>
2021-02-13 02:23:37 -0500

    added function to control randomization / able to get it working just need to figure out how to repeat all code on button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,52 @@
 // Assignment code here
 
 
+// Prompts for user input
+window.alert("Welcome to my Password Generator");
+var randoInput = window.prompt("How many characters would you like your password to be?");
+var b = parseInt(randoInput);
+var symbVerify = confirm("Would you like to include random symbols? OK for 'Yes' or CANCEL for 'NO'");
+var charUpVerify = confirm("Would you like to include random UPPER case letters? OK for 'Yes' or CANCEL for 'NO'");
+var numVerify = confirm("Would you like to include random Numbers? OK for 'Yes' or CANCEL for 'NO'");
+var charLwrVerify = confirm("Would you like to include random LOWER case letters? OK for 'Yes' or CANCEL for 'NO'");
+
+// Defined Arrays
+var passCharUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
+var passCharLwr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var passNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var passSym = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+
+const generatePassword = (length = b) => {
+  // Declare all characters
+  let chars = '';
+
+  console.log(charUpVerify);
+
+  if (charUpVerify) {
+    chars = passCharUpper.join('');
+  }
+  if (charLwrVerify) {
+    chars = chars.concat(passCharLwr.join(''));
+  }
+  if (numVerify) {
+    chars = chars.concat(passNum.join(''));
+  }
+  if (symbVerify) {
+    chars = chars.concat(passSym.join(''));
+  }
+  console.log(chars);
+  // Pick characers randomly
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return str;
+
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -11,30 +57,10 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-debugger;
-function generatePassword() {
-  window.alert("Please Select Criteria during the next prompts");
-  var charactersAZ = prompt("How many characters do you want in your new password?" + "\n" + "Please choose a number of at least 8 and no more that 128");
-  var symbols = confirm("Would you like symbols in your password?");
-
-  if (charactersAZ < 8 || charactersAZ > 128) {
-    window.prompt("Please enter between 8 and 128!");
-  } else {
-    window.prompt("Please enter a number!");
-
-  }
-  if (symbols) {
-    window.alert("Approved Symbols: " + "\n" + " '!#$%&()*+,-./:;<=>?@[\]^_`{|}~" + "\n" + "Will be added to your password");
-  }
-  else {
-    window.alert("No symbols will be added to your password");
-  }
 
 
-
-}
 
